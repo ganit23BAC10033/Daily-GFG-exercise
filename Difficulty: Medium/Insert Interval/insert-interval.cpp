23ea8d962898d062877336f707ef40ac14b79e77
Ganit Yadav <ganityadav07@gmail.com>
2024-12-09 19:05:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Insert Interval/insert-interval.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+public:
+    vector<vector<int>> insertInterval(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        int n = intervals.size();
+        vector<vector<int>> ans;
+
+        // Add the new interval to the list and sort
+        intervals.push_back(newInterval);
+        sort(intervals.begin(), intervals.end());
+
+        // Initialize the result with the first interval
+        ans.push_back(intervals[0]);
+
+        // Merge overlapping intervals
+        for (int i = 1; i <= n; i++) { // Start from the second interval
+            if (ans.back()[1] >= intervals[i][0]) {
+                // Merge intervals if they overlap
+                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
+            } else {
+                // Add a non-overlapping interval
+                ans.push_back(intervals[i]);
+            }
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        vector<vector<int>> intervals(N, vector<int>(2));
+        for (int i = 0; i < N; i++) {
+            cin >> intervals[i][0] >> intervals[i][1];
+        }
+        vector<int> newInterval(2);
+        cin >> newInterval[0] >> newInterval[1];
+
+        Solution obj;
+        vector<vector<int>> ans = obj.insertInterval(intervals, newInterval);
+        cout << "[";
+        for (int i = 0; i < ans.size(); i++) {
+            cout << "[" << ans[i][0] << ',' << ans[i][1] << ']';
+            if (i != (ans.size() - 1))
+                cout << ",";
+        }
+        cout << "]" << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
