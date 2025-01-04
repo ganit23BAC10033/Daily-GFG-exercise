4f95482eb6d5d2d0dd27c48c1775d984aa848c6f
Ganit Yadav <ganityadav07@gmail.com>
2025-01-04 16:50:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count all triplets with given sum in sorted array/count-all-triplets-with-given-sum-in-sorted-array.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int countTriplets(vector<int> &arr, int target) {
+        int count = 0;
+        int n = arr.size();
+        
+        // Iterate through all possible triplets
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (arr[i] + arr[j] + arr[k] == target) {
+                        count++;
+                    }
+                }
+            }
+        }
+        
+        return count;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+vector<int> lineArray() {
+    string line;
+    getline(cin, line);
+    stringstream ss(line);
+    vector<int> arr;
+    int num;
+    while (ss >> num) {
+        arr.push_back(num);
+    }
+    return arr;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr = lineArray();
+        int target;
+        cin >> target;
+        cin.ignore();
+
+        Solution ob;
+        int res = ob.countTriplets(arr, target);
+        cout << res << endl;
+        cout << "~" << endl;
+    }
+}
+
+// } Driver Code Ends
